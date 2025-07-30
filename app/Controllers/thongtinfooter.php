99f734cc3f9b3377d99ba5557d8baf9e7910b73c
Nguyen Dinh Van <nguyendinhvancs@gmail.com>
2025-07-30 15:10:17 +0700

    chinh sua footer

---
+++ app/Controllers/thongtinfooter.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+class thongtinfooter
+{
+    public function huongDanMuaHang()
+    {
+        view('thong-tin-footer/huong_dan_mua_hang');
+    }
+
+    public function doiTraSanPham()
+    {
+        view('thong-tin-footer/doi_tra_san_pham');
+    }
+
+    public function baoHanh()
+    {
+        view('thong-tin-footer/chinh_sach_bao_hanh');
+    }
+
+    public function hinhThucThanhToan()
+    {
+        view('thong-tin-footer/hinh_thuc_thanh_toan');
+    }
+}
--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -169,12 +169,17 @@
                     <div class="flex space-x-3">
                         <a href="#" class="text-muted-foreground hover:text-foreground">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M22 12c0-5.522-4.477-10-10-10S2 6.478 2 12c0 4.991 3.656 9.128 8.438 9.878v-6.988H7.898V12h2.54V9.797c0-2.507 1.492-3.89 3.777-3.89 1.094 0 2.238.196 2.238.196v2.46h-1.261c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33V21.878C18.344 21.128 22 16.991 22 12z" />
+                            </svg>
+                        </a>
+                        <a href="#" class="text-muted-foreground hover:text-foreground">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                                 <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
                             </svg>
                         </a>
                         <a href="#" class="text-muted-foreground hover:text-foreground">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M22.46 6c-.77.35-1.6.58-2.46.69.88-.53 1.56-1.37 1.88-2.38-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29 0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15 0 1.49.75 2.81 1.91 3.56-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07 4.28 4.28 0 0 0 4 2.98 8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21 16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56.84-.6 1.56-1.36 2.14-2.23z" />
+                                <path d="M20.447 20.452H17.2V14.8c0-1.348-.027-3.082-1.88-3.082-1.881 0-2.169 1.466-2.169 2.982v5.752H9.904V9h3.114v1.561h.044c.433-.82 1.492-1.683 3.071-1.683 3.285 0 3.89 2.162 3.89 4.974v6.6zM5.337 7.433a1.808 1.808 0 1 1 0-3.616 1.808 1.808 0 0 1 0 3.616zM6.787 20.452H3.886V9h2.901v11.452zM22.225 0H1.771C.792 0 0 .772 0 1.723v20.549C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.272V1.723C24 .772 23.2 0 22.222 0z" />
                             </svg>
                         </a>
                     </div>
@@ -195,10 +200,10 @@
                 <div class="space-y-4">
                     <h4 class="font-semibold text-foreground">🤝 Hỗ Trợ Khách Hàng</h4>
                     <ul class="space-y-2 text-sm">
-                        <li><a href="/huong-dan-mua-hang" class="text-muted-foreground hover:text-foreground">Hướng Dẫn Mua Hàng</a></li>
-                        <li><a href="/bao-hanh" class="text-muted-foreground hover:text-foreground">Chính Sách Bảo Hành</a></li>
-                        <li><a href="/doi-tra" class="text-muted-foreground hover:text-foreground">Đổi Trả Sản Phẩm</a></li>
-                        <li><a href="/hinh-thuc-thanh-toan" class="text-muted-foreground hover:text-foreground">Hình Thức Thanh Toán</a></li>
+                        <li><a href="/huong-dan-mua-hang" class="text-muted-foreground hover:text-foreground">📖 Hướng Dẫn Mua Hàng</a></li>
+                        <li><a href="/bao-hanh" class="text-muted-foreground hover:text-foreground">📌 Chính Sách Bảo Hành</a></li>
+                        <li><a href="/doi-tra" class="text-muted-foreground hover:text-foreground">🔄 Đổi Trả Sản Phẩm</a></li>
+                        <li><a href="/hinh-thuc-thanh-toan" class="text-muted-foreground hover:text-foreground">💳 Hình Thức Thanh Toán</a></li>
                     </ul>
                 </div>
 
